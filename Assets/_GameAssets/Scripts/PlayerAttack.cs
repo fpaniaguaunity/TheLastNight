1c122aea898ac9db5cf6b9956514c4eeb66ddd5e
Fernando Paniagua Formación <fernando.paniagua.unity@gmail.com>
2024-04-11 19:36:54 +0200

    Intermedio

---
+++ Assets/_GameAssets/Scripts/PlayerAttack.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public GameObject proyectil;
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")){
+            animator.SetTrigger("Attack");
+        }
+    }
+}
